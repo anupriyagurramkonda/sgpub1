7275566f466ab37cc8b8abb2ffe8d3df40bf9aee
anupriyagurramkonda <50578832+anupriyagurramkonda@users.noreply.github.com>
2019-08-29 11:38:45 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,30 @@
+variable "region"{
+ 
+}
+variable "aws_access_key"{
+
+}
+
+variable "aws_secret_key"{
+
+}
+
+variable "name"{
+
+}
+
+variable "from_port"{
+
+}
+
+variable "to_port"{
+
+}
+
+variable "protocol"{
+
+}
+
+variable "cidr_blocks"{
+ 
+}
