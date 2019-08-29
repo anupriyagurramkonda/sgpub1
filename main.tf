8d3ea0e155c765461940c3cd81a1d6fa6c1b150b
anupriyagurramkonda <50578832+anupriyagurramkonda@users.noreply.github.com>
2019-08-29 14:46:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,6 @@ resource "aws_security_group" "example" {
  
     name = "${var.name}"
 
- ingress {
-    from_port       = "${var.ib_from_port}"
-    to_port         = "${var.ib_to_port}"
-    protocol        = "${var.ib_protocol}"
-    cidr_block      = ["${var.ib_cidr_block}"]
-   }
   egress {
     from_port       = "${var.ob_from_port}"
     to_port         = "${var.ob_to_port}"
